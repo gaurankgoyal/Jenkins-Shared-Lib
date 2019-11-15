525a033a79e5f8cd16564fb178241ff2d34cf469
gaurank goyal <gaurank25@gmail.com>
2019-11-15 16:44:26 +0530

    update

---
+++ vars/commonFun.groovy
@@ -0,0 +1,17 @@
+def setJobProperties(numToKeep, pollSCMSchedule) {
+    def jobProperties = []
+    jobProperties.add(disableConcurrentBuilds())
+    if (numToKeep == null || numToKeep.isEmpty()) {
+        numToKeep = '10'
+    }
+    // Set Build retention
+    jobProperties.add([$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: numToKeep]],)
+    if (pollSCMSchedule == null || pollSCMSchedule.isEmpty()) {
+        pollSCMSchedule = 'H/30 * * * *' // By default, poll SCM every 30 minitues
+    }
+    // Set Poll SCM schedule
+    else {
+        jobProperties.add(pipelineTriggers([[$class: 'GitHubPushTrigger'], pollSCM(pollSCMSchedule)]))
+    }
+    properties(jobProperties)
+}
