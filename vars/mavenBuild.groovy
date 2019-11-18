873a15c3343ec7094f2141d41e3173484e284458
gaurank goyal <gaurank25@gmail.com>
2019-11-18 11:34:58 +0530

    update

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -9,11 +9,12 @@ def call(body) {
     commonFun.setJobProperties(env.NUM_BUILDS_KEPT, "H/10 * * * *")
     SERVER_URL = commonFun.artifactoryServerUrl()
     CREDENTIALS = "atrifactory"
-    artifactoryServer = Artifactory.newServer url: SERVER_URL, credentialsId: CREDENTIALS
-    print (artifactoryServer)
-    rtMaven = Artifactory.newMavenBuild()
-    print (rtMaven)
-    rtMaven.tool = mavenTool
+    def artifactoryServer = Artifactory.newServer url: SERVER_URL, credentialsId: CREDENTIALS
+    //print (artifactoryServer)
+    def rtMaven = Artifactory.newMavenBuild()
+    //print (rtMaven)
+    def buildInfo
+    //rtMaven.tool = mavenTool
     print (SERVER_URL) 
     print (CREDENTIALS)
     stage 'checkout'
@@ -22,16 +23,16 @@ def call(body) {
         	checkout scm
 
 	}
-	rtMaven.deployer releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local', server: artifactoryServer
-        rtMaven.resolver releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot', server: artifactoryServer
-        buildInfo = Artifactory.newBuildInfo()
-	pomVersion = readMavenPom().getVersion()
-	println(pomVersion)
+	stage ('Artifactory configuration') {
+		rtMaven.tool = mavenTool
+		rtMaven.deployer releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local', server: artifactoryServer
+        	rtMaven.resolver releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot', server: artifactoryServer
+        	buildInfo = Artifactory.newBuildInfo()
+		//pomVersion = readMavenPom().getVersion()
+		//println(pomVersion)
+	}
         stage 'Build'
-        //docker.image(config.environment).inside {
-        	//sh config.mainScrit
 		rtMaven.run pom: 'pom.xml', goals: 'clean install', buildInfo: buildInfo
-        //}
         stage 'UnitTest'
 	stage ('Publish build info') {
         artifactoryServer.publishBuildInfo buildInfo
