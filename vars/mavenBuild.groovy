2efb9b44323ff0ee21f6e2b6524c6e774e0793fe
gaurank goyal <gaurank25@gmail.com>
2019-11-18 12:04:18 +0530

    update

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,4 +1,3 @@
-import com.infosys.JenkinsAddUsernameWithPasswordCred
 
 def call(body) {
     def config = [:]
@@ -20,7 +19,47 @@ def call(body) {
     print (SERVER_URL) 
     print (CREDENTIALS)
 
-    jenkinsKeyUsernameWithPassword()
+
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.Domain
+import com.cloudbees.plugins.credentials.impl.*
+import hudson.util.Secret
+import jenkins.model.Jenkins
+
+// parameters
+def jenkinsKeyUsernameWithPasswordParameters = [
+  description:  'Description here',
+  id:           'key-id-here',
+  secret:       '12345678901234567890',
+  userName:     'your-username-here'
+]
+
+// get Jenkins instance
+Jenkins jenkins = Jenkins.getInstance()
+
+// get credentials domain
+def domain = Domain.global()
+
+// get credentials store
+def store = jenkins.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+// define Bitbucket secret
+def jenkinsKeyUsernameWithPassword = new UsernamePasswordCredentialsImpl(
+  CredentialsScope.GLOBAL,
+  jenkinsKeyUsernameWithPasswordParameters.id,
+  jenkinsKeyUsernameWithPasswordParameters.description,
+  jenkinsKeyUsernameWithPasswordParameters.userName,
+  jenkinsKeyUsernameWithPasswordParameters.secret
+)
+
+// add credential to store
+store.addCredentials(domain, jenkinsKeyUsernameWithPassword)
+
+// save to disk
+jenkins.save()
+
+
+
 
     stage 'checkout'
     node {
